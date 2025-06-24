c66ceefe69c974f2745a0148664d1d03e44c6f80
user-acer <acer@gmail.com>
2025-06-24 13:56:04 +0700

    add fitur pencarian

--- components/PengajuanTable.jsx
+++ components/PengajuanTable.jsx
@@ -1,12 +1,21 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { motion } from "framer-motion";
 import { Edit, Search, Trash, Trash2 } from "lucide-react";
 import Image from "next/image";
 
 const ProductsTable = () => {
   const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredProducts = useMemo(() => {
+    return products.filter(
+      (product) =>
+        product.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
+        product.category.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+    );
+  });
 
   useEffect(() => {
     fetch("/data/data.json")
@@ -34,7 +43,9 @@ const ProductsTable = () => {
         <div className="relative w-full md:w-auto">
           <input
             type="text"
-            placeholder="cari pengajuan"
+            placeholder="cari pengajuan.."
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
             className="bg-[#2f2f2f] text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 w-full md:w-64 focus:outline-none focus:ring-2 focus:ring-gray-500 transition duration-200 text-sm"
           />
 
@@ -66,7 +77,7 @@ const ProductsTable = () => {
           </thead>
 
           <tbody className="divide-y divide-gray-700">
-            {products.map((product) => (
+            {filteredProducts.map((product) => (
               <motion.tr
                 key={product.id}
                 initial={{ opacity: 0, y: 10 }}
